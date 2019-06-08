0d3534af101f64a48e00a94a99cc4e46f7553a98
gburgardt <germanandresburgardt@gmail.com>
2019-06-08 00:51:13 -0300

    Punto Muestral

--- server.js
+++ server.js
@@ -31,6 +31,30 @@ app.get(
             )
 );
 
+app.get(
+    '/punto_muestral/:celular', 
+    (req, res) => {
+
+        const celular = req.params ? req.params.celular : null;
+
+        // Si SI tiene celular, filtro por el. Caso contrario, busco todos los punto_muestral
+        if (celular) {
+            return knex('punto_muestral').select('*')
+                .where('celular', celular)
+                .then(
+                    resp => res.send(resp)
+                )
+
+        } else {
+            return knex('mesa').select('*')
+                .then(
+                    resp => res.send(resp)
+                )
+        }
+
+    }
+);
+
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
 });
