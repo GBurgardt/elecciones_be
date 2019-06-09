7850ae2e9217433596652eace03d66074ca9c1ad
German Burgardt <germanandresburgardt@gmail.com>
2019-06-09 18:02:21 -0300

    subir img

--- server.js
+++ server.js
@@ -4,36 +4,35 @@ const knex = require('./knex/knex.js');
 const cors = require('cors')
 const app = express();
 
+
+/**
+ * Config multer
+ */
+const multer = require('multer');
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            console.log("Dest");
+            cb(null, 'upload/')
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.fieldname + '-' + Date.now())
+        }
+    })
+})
+
 /**
  * Configura las CORS
  */
 app.use(cors())
 
-
 /**
  * Arrancan endpoints
  */
-app.get(
-    '/categorias', 
-    (req, res) => 
-        knex('categoria').select('*')
-            .then(
-                resp => res.send(resp)
-            )
-);
 
 app.get(
-    '/mesas/:idPuntoMuestral', 
-    (req, res) => 
-        knex('mesa').select('*')
-            .where('idpuntomuestral', req.params.idPuntoMuestral)
-            .then(
-                resp => res.send(resp)
-            )
-);
-
-app.get(
-    '/punto_muestral/:celular', 
+    '/punto_muestral/:celular',
     (req, res) => {
 
         const celular = req.params ? req.params.celular : null;
@@ -55,6 +54,47 @@ app.get(
 
     }
 );
+
+
+app.get(
+    '/punto_muestral/:idPuntoMuestral/mesas',
+    (req, res) =>
+        knex('mesa').select('*')
+            .where('idpuntomuestral', req.params.idPuntoMuestral)
+            .then(
+                resp => res.send(resp)
+            )
+);
+
+
+app.get(
+    '/categoria',
+    (req, res) =>
+        knex('categoria').select('*')
+            .then(
+                resp => res.send(resp)
+            )
+);
+
+app.get(
+    '/categoria/:idCategoria/candidatos',
+    (req, res) =>
+        knex('candidato').select('*')
+            .where('idcategoria', req.params.idCategoria)
+            .then(
+                resp => res.send(resp)
+            )
+);
+
+
+app.post('/test-upload', upload.single('attachment'), (req, res, next) => {
+    res.send({ status: 'ok' })
+})
+
+
+/**
+ * Fin endpoints
+ */
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
