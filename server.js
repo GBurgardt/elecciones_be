cb35e8ea98aef7b2e80bff140ff9190fa84cb4d2
German Burgardt <germanandresburgardt@gmail.com>
2019-06-10 01:11:15 -0300

    Reglas

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ const upload = multer({
             cb(null, 'upload/')
         },
         filename: function (req, file, cb) {
-            cb(null, file.fieldname + '-' + Date.now())
+            const idMesa = req.params.idMesa;
+            const idCategoria = req.params.idCategoria;
+
+            cb(null, `mesa-${idMesa}_categoria-${idCategoria}_date-${Date.now()}`)
         }
     })
 })
@@ -66,14 +69,53 @@ app.get(
             )
 );
 
-
+/**
+ * Filtro las categorias DE ESA MESA que ya se han votado
+ *  */
 app.get(
-    '/categoria',
-    (req, res) =>
-        knex('categoria').select('*')
+    '/punto_muestral/:idPuntoMuestral/mesas/:idMesa/categorias',
+    (req, res) => 
+
+        knex('categoria').select('idcategoria').distinct()
+            .join('candidato', 'candidato.idcategoria', '=', 'categoria.id')
+            .join('mesa_candidato', function () {
+                this
+                    .on('mesa_candidato.idcandidato', '=', 'candidato.id')
+                    .andOn('mesa_candidato.idmesa', '=', req.params.idMesa)
+            })
+            .join('mesa', 'mesa.id', '=', 'mesa_candidato.idmesa')
+            .where(function () {
+                this
+                    .where('mesa.id', req.params.idMesa)
+                    .andWhere('mesa.idpuntomuestral', req.params.idPuntoMuestral)
+            })
             .then(
-                resp => res.send(resp)
+                categoriasCargadas => 
+                    knex('categoria').select('*')
+                        .whereNotIn('id', categoriasCargadas)
+                        .then(
+                            resp => res.send(resp)
+                        )
             )
+        
+//     select distinct categoria.id from categoria
+//         inner join candidato 
+//             on candidato.idcategoria = categoria.id
+//         inner join mesa_candidato
+//             on (
+//                 mesa_candidato.idcandidato = candidato.id and
+//                 mesa_candidato.idmesa = 1
+//             )
+//         inner join mesa 
+//             on mesa.id = mesa_candidato.idmesa
+//     where     
+//         mesa.id = 1 and 
+//         mesa.idpuntomuestral = 1
+
+    
+
+    
+
 );
 
 app.get(
@@ -90,6 +132,54 @@ app.get(
 app.post('/test-upload', upload.single('attachment'), (req, res, next) => {
     res.send({ status: 'ok' })
 })
+
+
+app.post(
+    '/mesa-candidato/:idMesa/:idCategoria',
+    (req, res, next) => {
+
+        // Primero checkeo que se suba bien la foto. Caso contrario cancelo todo
+        upload.single('attachment')(req, res, function (err) {
+            if (err instanceof multer.MulterError) {
+                return res.status('404'.send(err))
+            } else if (err) {
+                return res.status('500'.send(err))
+            }
+
+            /**
+             * Mapeo el body a un array de promises (todos los insert a la db) que despues resuelvo todos juntos
+             */
+            const mesasCandidatos = JSON.parse(req.body['mesasCandidatos'])
+                .map(
+                    mc => ({
+                        idmesa: mc.mesa.id,
+                        idcandidato: mc.candidato.id,
+                        cantidadvotos: mc.cantidadVotos
+                    })
+                )
+                .map(
+                    m => knex('mesa_candidato').insert(m)
+                )
+
+
+            Promise.all(mesasCandidatos)
+                .then(
+                    resp => res.send({
+                        status: 'ok'
+                    })
+                )
+                .catch(
+                    err => res.status(404).send({
+                        status: 'error',
+                        body: err
+                    })
+                )
+
+        })
+
+
+    }
+)
 
 
 /**
